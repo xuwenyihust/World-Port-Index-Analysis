23e1100be130e61b2382c39a9a13e4ef3d339c0b
xuwenyihust <wenyixu101@gmail.com>
2022-10-03 20:37:01 +0800

    Complete question 3.

---
+++ src/distance.py
@@ -0,0 +1,17 @@
+from math import radians, cos, sin, asin, sqrt
+
+
+def get_distance(lat1, lat2, lon1, lon2):
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+
+    c = 2 * asin(sqrt(a))
+    r = 6371
+
+    return c * r * 1000
